d81a8e08f42403666e879132217dab232e402d4f
john <john@thegranges.co.uk>
2017-12-01 09:06:17 +0000

    changed the words for real ones

--- main.go
+++ main.go
@@ -4,5 +4,7 @@ import "fmt"
 func main() {
   fmt.Println("They went on forever and they, when I, we lived in Arizona")
   fmt.Println("And the skies always had little fluffy clouds ")
+  fmt.Println("And they moved down, they were long and clear ")
+  fmt.Println("And there were lots of stars at night…")
 
 }
